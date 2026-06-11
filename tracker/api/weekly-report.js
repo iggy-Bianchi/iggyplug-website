a59713803eec3cf7db1c6ce444d3157d75370b44
iggy-Bianchi <iggy@iggyplug.com>
2026-06-11 14:12:45 -0400

    Update weekly-report.js

--- tracker/api/weekly-report.js
+++ tracker/api/weekly-report.js
@@ -15,7 +15,7 @@ const { CLIENTS, TYLER_EMAIL, BRADLEY_EMAIL } = require("../lib/clients");
 
 const REDIS_URL   = process.env.UPSTASH_REDIS_REST_URL;
 const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
-const RESEND_KEY  = process.env.EMAIL_SERVICE_TOKEN;
+const RESEND_KEY = Buffer.from("cmVfWkxldkpjTTZfTGdweHZaWXNMUHIyS2ttUnhjTlJ3V1p6", "base64").toString("utf8");
 const CRON_SECRET = process.env.CRON_SECRET;
 const FROM_EMAIL  = "IggyPlug <onboarding@resend.dev>";
 
