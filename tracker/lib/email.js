d8e1fc873f06c7afdc7f2f5862fc4e137d57d1d0
iggy-Bianchi <iggy@iggyplug.com>
2026-06-11 14:39:31 -0400

    Update email.js

--- tracker/lib/email.js
+++ tracker/lib/email.js
@@ -106,7 +106,7 @@ function buildEmailHTML(date, results, previousResults) {
 
 async function sendReport(date, results, previousResults) {
   const html = buildEmailHTML(date, results, previousResults);
-  const RESEND_KEY = "re_LFfhZD5i_CzLkrTigNTVLBPkJGMqxTK9n";
+  const RESEND_KEY = Buffer.from(process.env.EMAIL_SERVICE_TOKEN, "base64").toString("utf8");
 
   const res = await fetch("https://api.resend.com/emails", {
     method: "POST",
